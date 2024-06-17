0ce2b32771e14c6f6e82bb8fa9b3e255b40467cd
Nolan <noluck589@gmail.com>
2024-06-17 07:36:35 -0700

    updated app versions, run scripts

--- nginx/run.sh
+++ nginx/run.sh
@@ -1,7 +1,8 @@
 docker run \
-    -p 8443:443 \
-    -p 8080:80 \
+    -p 443:443 \
+    -p 80:80 \
     -v D:\\Development\\Projects\\finance\\stock_analysis_docker\\nginx\\server.pem:/etc/nginx/ssl/server.pem \
     -v D:\\Development\\Projects\\finance\\stock_analysis_docker\\nginx\\server.key:/etc/nginx/ssl/server.key \
     -v D:\\Development\\Projects\\finance\\stock_analysis_docker\\nginx\\nginx.conf:/etc/nginx/nginx.conf:ro \
+    --env-file D:\\Development\\Projects\\finance\\stock_analysis_panel\\.env \
     nginx
--- panel/run.sh
+++ panel/run.sh
@@ -1,1 +1,5 @@
-docker run --env-file ../../stock_analysis_panel/.env -p 8500:8500 703178115275.dkr.ecr.us-west-2.amazonaws.com/analysis_panel:v1
+docker run \
+    -p 8500:8500 \
+    --env-file ../../stock_analysis_panel/.env \
+    -v D:\\Development\\Projects\\finance\\stock_analysis_panel\\data:/srv/stock_analysis_panel/data \
+    703178115275.dkr.ecr.us-west-2.amazonaws.com/analysis_panel:v1
