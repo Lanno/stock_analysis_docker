cceca21f33fea1bdbedd26caab1672b1b7fad366
Nolan Luckett <noluck589@gmail.com>
2024-06-17 14:34:33 +0000

    modifications to run on ubuntun on aws

--- nginx/run.sh
+++ nginx/run.sh
@@ -1,7 +1,8 @@
 docker run \
-    -p 8443:443 \
-    -p 8080:80 \
-    -v D:\\Development\\Projects\\finance\\stock_analysis_docker\\nginx\\server.pem:/etc/nginx/ssl/server.pem \
-    -v D:\\Development\\Projects\\finance\\stock_analysis_docker\\nginx\\server.key:/etc/nginx/ssl/server.key \
-    -v D:\\Development\\Projects\\finance\\stock_analysis_docker\\nginx\\nginx.conf:/etc/nginx/nginx.conf:ro \
+    -d --rm \
+    -p 443:443 \
+    -p 80:80 \
+    -v $(pwd)/server.pem:/etc/nginx/ssl/server.pem \
+    -v $(pwd)/server.key:/etc/nginx/ssl/server.key \
+    -v $(pwd)/nginx.conf:/etc/nginx/nginx.conf:ro \
     nginx
--- nginx/test.sh
+++ nginx/test.sh
@@ -1,1 +1,7 @@
-docker run -v D:\\Development\\Projects\\finance\\stock_analysis_docker\\nginx\\nginx.conf:/etc/nginx/nginx.conf:ro -p 8080:80 nginx nginx -t
+docker run \
+    -p 8443:443 \
+    -p 8080:80 \
+    -v $(pwd)/server.pem:/etc/nginx/ssl/server.pem \
+    -v $(pwd)/server.key:/etc/nginx/ssl/server.key \
+    -v $(pwd)/nginx.conf:/etc/nginx/nginx.conf:ro \
+    nginx nginx -t
