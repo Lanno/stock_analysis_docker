17585ae750129ee0de86b9a9e9dc3e8b45338289
Nolan <noluck589@gmail.com>
2024-06-17 09:26:41 -0700

    updated panel app, use 8501 internally

--- panel/run.sh
+++ panel/run.sh
@@ -1,5 +1,5 @@
 docker run \
-    -p 8500:8500 \
+    -p 8500:8501 \
     --env-file ../../stock_analysis_panel/.env \
     -v D:\\Development\\Projects\\finance\\stock_analysis_panel\\data:/srv/stock_analysis_panel/data \
     703178115275.dkr.ecr.us-west-2.amazonaws.com/analysis_panel:v1
